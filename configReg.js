4f7394d8c90366849d0d1fb4ccc52e4c5eb8b2f5
Hank Guo <hguo@walmart.com>
2018-01-15 23:42:45 +0800

    Add config reg test

--- configReg.js
+++ configReg.js
@@ -2,18 +2,30 @@ const typeMap = {
   cf: 'config',
   cl: 'color',
 };
+const configRegExp = /[:=]\s*"?([^",]+)"?\s*,?\s*\/\/(c[fl])[._]([^.][^,"]+[^.]+);\s*/gi;
 const parse = (str) => {
-  const [, value, type, key] = /[:=]\s*"?([^",]+)"?\s*,?\s*\/\/(c[fl])[.-]([^;-]+);\s*/gi.exec(
+  const [, value, type, key] = new RegExp(configRegExp).exec(
       str) || [];
-  return {key, type: typeMap[type], value};
+  return {key: key && key.trim(), type: typeMap[type], value: value && value.trim()};
+};
+
+const res = {
+  color: {},
+  config: {}
 };
 
 [
-  ': "#222222" , //cl.red ;',
-  ': 3, //cf.test.number ;',
+  'sss: "#222222" , //cl.page.red ;ssss',
+  ': 3, //cf.test.number;',
+  ': 4, //cf.test.number;',
 ].forEach((e) => {
   const {value, key, type} = parse(e);
   if (key && value) {
     console.log(type, key, value);
+    if (res[type][key] != value) {
+      res[type][key] =  value * 1 ? value * 1 : value;
+    }
   }
 });
+
+require("fs").writeFileSync("configRes.json", JSON.stringify(res, null, 4));
